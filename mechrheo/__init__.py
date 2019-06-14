319d9f8c3ae1f07be3d7960e6d864ed743d48194
Jelle Spijker <j.spijker@ultimaker.com>
2019-06-14 14:19:30 +0200

    updated setup and requirements

--- mechrheo/__init__.py
+++ mechrheo/__init__.py
@@ -1,4 +1,10 @@
-from mechrheo.fitdata import FitData
-from mechrheo import models
+__author__ = """Jelle Spijker"""
+__email__ = 'spijker.jelle@gmail.com'
+__version__ = '0.1.0'
 
-__all__ = ['FitData', 'models']
+from pint import UnitRegistry, set_application_registry
+
+ureg = UnitRegistry(autoconvert_offset_to_baseunit=True, default_as_delta=False)
+ureg.setup_matplotlib(True)
+Q_ = ureg.Quantity
+set_application_registry(ureg)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'scipy']
+requirements = ['numpy', 'scipy', 'pint', 'matplotlib']
 
 setup_requirements = ['pytest-runner', ]
 
